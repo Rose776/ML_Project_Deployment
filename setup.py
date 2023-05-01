d7628f90e12a9f7b2b8e839e047fca7ddf134d2f
Rose776 <19bit79@americancollege.edu.in>
2023-05-02 00:14:57 +0530

    First commit

--- setup.py
+++ setup.py
@@ -30,3 +30,7 @@ install = get_requirement('requirements.txt')
 
 )
 
+""" git commit -m "First commit"
+ git config --global user.email "EMAIL"
+ git config --global user.name Rose776
+ git push -u origin main"""
